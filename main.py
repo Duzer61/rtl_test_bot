24bdf72b6063702cf2f1d206af91fe2c14d20f79
Данил Кочетов <dmi194@yandex.ru>
2024-03-19 19:12:07 +0300

    started main

--- main.py
+++ main.py
@@ -3,9 +3,24 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
+from config_data.config import config
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
     format='%(filename)s:%(lineno)d #%(levelname)-8s '
            '[%(asctime)s] - %(name)s - %(message)s'
 )
+
+
+async def main():
+    logger.info('Starting bot')
+    bot = Bot(token=config.tg_bot.token,
+              parse_mode='HTML')
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
