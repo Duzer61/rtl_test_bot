91017a16cc30cc945c0c9e8ae54fa0dbc14d7c6a
Данил Кочетов <dmi194@yandex.ru>
2024-03-19 18:54:44 +0300

    Added config

--- config_data/config.py
+++ config_data/config.py
@@ -1,1 +1,37 @@
-pass
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Database:
+    base_name: str
+    collection_name: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    database: Database
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(
+        tg_bot=TgBot(
+            token=env('BOT_TOKEN')
+        ),
+        database=Database(
+            base_name=env('BASE_NAME'),
+            collection_name=env('COLLECTION_NAME'),
+        )
+    )
+
+
+config: Config = load_config()
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-pass
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(filename)s:%(lineno)d #%(levelname)-8s '
+           '[%(asctime)s] - %(name)s - %(message)s'
+)
