4f721dd2861f118b9ebebd59be12c6819188a4b1
Данил Кочетов <dmi194@yandex.ru>
2024-03-19 19:38:27 +0300

    add mongo

--- database/mongo_db.py
+++ database/mongo_db.py
@@ -1,1 +1,7 @@
-pass
+from pymongo import MongoClient
+
+from config_data.config import config
+
+client: MongoClient = MongoClient('mongodb://localhost:27017')
+db = client[config.database.base_name]
+collection = db[config.database.collection_name]
